17839eac52193eedc80561414abef0a49ad413f8
Corentin <corentindavion@gmail.com>
2026-05-13 22:26:10 +0200

    added the function to solve to check if the 3x3 grid is valid

--- solve.py
+++ solve.py
@@ -8,32 +8,50 @@ class SudokuSolver :
         self.grid = grid
 
     
-    def is_cell_empty(self, i, j):
+    def is_cell_empty(self, i:int, j:int):
         return self.grid[i][j] == 0
 
 ### ici il y a des choses à changer ###
-    def row_is_valid(self, i):
+    def row_is_valid(self, i:int):
+        assert i<=8 and i>=0
         row = self.grid[i]
         if 0 in row:
             return True
         return np.sum(row) == 45 and len(set(row)) == 9
     
 
-    def column_is_valid(self, j):
+    def column_is_valid(self, j:int):
+        assert j<=8 and j>=0
         column = self.grid[:, j]
         if 0 in column:
             return True
         return np.sum(column) == 45 and len(set(column)) == 9
 
+    def three_by_three_is_valid(self,i):
+        
+        '''
+        Thre three by three are numbered as follows:
+        0 1 2
+        3 4 5
+        6 7 8
+        '''
+        assert i<=8 and i>=0
+        temp=[] #to gather the respective element in the grid
+        index_i,index_j= i//3, i%3 #get which three by three it is
+        for m in range (index_i*3,index_i*3+3): #get the rows index
+            for n in range(index_j*3,index_j*3+3): #get the col index
+                temp.append(self.grid[m,n])
+        return np.sum(np.array(temp))==45 and len(set(temp))==9
 ### fin des choses à changer ###
 
 
     def sudoku_is_valid(self):
         for i in range(9):
-            if not self.row_is_valid(i) or not self.column_is_valid(i):
+            if not self.row_is_valid(i) or not self.column_is_valid(i) or not self.three_by_three_is_valid(i):
                 return False
         return True
     
+
 
     def placeable_digits(self, i, j):
         if not self.is_cell_empty(i, j):
